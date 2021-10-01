77eff4caf0bb9543229267fef69737b68836bd25
Lucas Nicolas Alberca <86059300+Capigol@users.noreply.github.com>
2021-10-01 16:36:08 -0300

    Update iRaPCA_v2.py

--- iRaPCA_v2.py
+++ iRaPCA_v2.py
@@ -89,7 +89,8 @@ The next workflow summarizes the steps performed by this method:
 image = Image.open('workflow_iRaPCA.png')
 st.image(image, caption='Clustering Workflow')
 
-st.markdown(:rocket: Fast tutorial [iRaPCA](https://www.youtube.com/watch?v=6HoBdFDY9Ic&ab_channel=LIDeBUNLP))
+st.markdown(":rocket:")
+st.write("Fast tutorial " [iRaPCA](https://www.youtube.com/watch?v=6HoBdFDY9Ic&ab_channel=LIDeBUNLP))
 
 #st.markdown("""
 #         **To cite the application, please reference XXXXXXXXX**
